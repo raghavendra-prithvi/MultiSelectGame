0e6defc195257645fe7df131ea92ac4dfbfcafa9
raghavendra-prithvi <raghava.sangars@gmail.com>
2013-08-23 02:19:08 +0530

    scores

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -238,7 +238,7 @@ class QuestionsController < ApplicationController
 
 
   def high_scores
-      @scores = Score.where(:user_id => session[:user_id]).order("points DESC")
+      @scores = Score.where(:user_id => session[:user_id]).order("points DESC").limit(10)
   end
   
   def game_start
@@ -269,12 +269,12 @@ class QuestionsController < ApplicationController
     puts params.inspect
    # @scores = Score.find_by_sql("SELECT DISTINCT(s.user_id), max(s.points) FROM scores s where s.user_id in #{params[:ids]} order by s.points desc")
     #@scores = Score.where(:user_id => params[:ids]).order("points DESC")
-    @scores = Score.where(:user_id => params[:ids]).group('user_id').order("max(points) DESC").limit(10)
+    @scores = Score.where(:user_id => params[:ids]).group('user_id,id').order("max(points) DESC").limit(10)
     render :html => "getFriendsData", :layout => false
   end
   def getGlobalData
     puts params.inspect
-    @scores = Score.limit(10).group('user_id').order("max(points) DESC")
+    @scores = Score.limit(10).group('user_id,id').order("max(points) DESC")
     render :html => "getGlobalData", :layout => false
   end
 
